61c2a3dd6b779572ee1b4c910d3973ed6255838d
Esben Lomholt Bjarnason <esbenlb@hotmail.com>
2021-08-06 13:18:48 +0200

    Added test api.

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/custom_Widget/item.dart';
+import 'package:shopping_app/api/google_api.dart';
+import 'package:shopping_app/api/recipe_api.dart';
 
 class History extends StatefulWidget {
   const History({Key? key}) : super(key: key);
@@ -12,13 +14,19 @@ class _HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('History          '),
+        appBar: AppBar(
+          title: Center(
+            child: Text('History          '),
+          ),
         ),
-      ),
-      body: Item(
-          <String>["Item_1", "Item_2", "Hala"], <String>["20", "30", "45"]),
-    );
+        //body: Item(
+        //    <String>["Item_1", "Item_2", "Hala"], <String>["20", "30", "45"]),
+        body: RecipeCard(
+          title: 'My recipe',
+          rating: '4.9',
+          cookTime: '30 min',
+          thumbnailUrl:
+              'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+        ));
   }
 }
