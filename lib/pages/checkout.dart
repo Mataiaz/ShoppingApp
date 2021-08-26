b57ae7c079835e4f73ae82d61f09c2d01d714b24
Mataiaz <mathias344@live.no>
2021-08-26 16:39:21 +0200

    setting up functions in the checkout page

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shopping_app/pages/settings.dart';
+import 'package:shopping_app/api/recipe_api.dart';
+import 'package:shopping_app/api/recipe_json.dart';
+import 'package:shopping_app/custom_Widget/recipe_card.dart';
+//import 'package:shopping_app/api/google_api.dart';
+import 'package:shopping_app/api/recipe_api.dart';
 
 class Checkout extends StatefulWidget {
   const Checkout({Key key}) : super(key: key);
@@ -9,16 +15,68 @@ class Checkout extends StatefulWidget {
 }
 
 class _CheckoutState extends State<Checkout> {
+  List<Recipe> _recipes = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipesFromFB();
+  }
+
+  Future<List<Recipe>> getRecipesFromFB() async {
+    List<Recipe> tmp = [];
+    var data = await FirebaseFirestore.instance
+        .collection('public')
+        .get()
+        .then((snapshot) => {
+              snapshot.docs.forEach((element) {
+                //print(element.data());
+                Recipe x = Recipe(
+                    name: element.data()["title"],
+                    images: element.data()["thumbnailUrl"],
+                    rating: 2,
+                    totalTime: element.data()["cookTime"]);
+                tmp.add(x);
+                _recipes.add(x);
+                print(element.data()["title"]);
+              })
+            })
+        .catchError((onError) {
+      print('Caught $onError');
+    }).whenComplete(() => {
+              print("HEY"),
+              //print(tmp[0]),
+              print(tmp),
+              setState(() {
+                _isLoading = false;
+              })
+            });
+
+    print("hei            sadffffffffffffffffffffffffffffff: ");
+
+    return tmp;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('CHECKOUT          '),
+        appBar: AppBar(
+          title: Center(
+            child: Text('Checkout'),
+          ),
         ),
-      ),
-      body: SafeArea(
-          child: Text('THIS IS WHERE WE WILL CHECKOUT ITEMS SELECTED')),
-    );
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _recipes.length,
+                itemBuilder: (context, index) {
+                  return RecipeCard(
+                      title: _recipes[index].name,
+                      cookTime: _recipes[index].totalTime,
+                      rating: _recipes[index].rating.toString(),
+                      thumbnailUrl: _recipes[index].images);
+                },
+              ));
   }
 }
